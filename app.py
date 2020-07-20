6d771429d35909b8d8cb0eda5ee82176eee0cc5c
mvmolloy <mvamolloy@gmail.com>
2020-07-20 16:22:10 +0000

    import session and flash from flask

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
