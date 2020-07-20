eb6bf4d7dd5bb557c4b9da42d5a2d460c0315004
mvmolloy <mvamolloy@gmail.com>
2020-07-20 15:10:31 +0000

    adding background to index carousel

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    /* index.html */
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+    setInterval(function(){
+        $('.carousel').carousel('next');
+    }, 10000);
+    });
