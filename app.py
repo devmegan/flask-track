05a388051c68e2dcfb6ce7aa9928a8820671f477
mvmolloy <mvamolloy@gmail.com>
2020-07-25 17:20:32 +0000

    adding a 404 route

--- app.py
+++ app.py
@@ -320,6 +320,13 @@ def logout():
     return redirect(url_for("index"))
 
 
+""" 404 ERROR """
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template("404.html"), 404
+
 """ helper functions """
 
 
