6f45806ee9c5fed129ecfd53ff6721df0aeccf79
mvmolloy <mvamolloy@gmail.com>
2020-07-31 20:55:17 +0000

    trying to fix divide by 0

--- app.py
+++ app.py
@@ -546,4 +546,4 @@ def app_goals_achieved(direction, value):
     return
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=False)
